f5caf66e02f4de0949a2f28f5fefbe646c0605c5
Corné van Klink <cvklink@gmail.com>
2022-09-02 14:08:09 +0200

    Added scan-ability to basic_hack

--- basic_hack.js
+++ basic_hack.js
@@ -1,1 +1,11 @@
+/* Basic Hacking Script for BitBurner */
 
+var result;
+
+export async function main(ns) {
+    ns.print("Starting script here");
+	result = await ns.scan();
+    ns.print(result);
+    ns.print("Script finished");
+    
+}
